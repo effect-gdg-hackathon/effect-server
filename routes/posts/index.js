ea8d8c06afb15737cd413c6bc37e054072a90dc8
songhwee1 <songhwee1@naver.com>
2022-06-25 19:52:15 +0900

    get posts search by category

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,8 +1,15 @@
 const express = require("express"); // module for express framework
+const { effectCollection } = require("../../db");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", (req, res) => {
-  res.sendStatus(200);
+router.get("/", async (req, res) => {
+  const category = req.query.category;
+
+  const effect = await effectCollection.find({category: category}).toArray()
+
+  res.json({
+    list : effect
+  }).status(200);
 });
 
 module.exports = router;
