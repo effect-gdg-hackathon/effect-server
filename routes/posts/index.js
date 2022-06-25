a5beb9ee78fb4b6795957b2458e01d40214d9871
songhwee1 <songhwee1@naver.com>
2022-06-25 20:58:55 +0900

    search by category, postIds, creatorId

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -3,9 +3,18 @@ const { effectCollection } = require("../../db");
 const router = express.Router({ mergeParams: true });
 
 router.get("/", async (req, res) => {
-  const category = req.query.category;
-
-  const effect = await effectCollection.find({category: category}).toArray()
+  let param;
+  let effect;
+  if (req.query.category !== undefined) {
+    param = req.query.category;
+    effect = await effectCollection.find({category: param}).toArray();
+  } else if (req.query.postIds !== undefined){
+    param = req.query.postIds;
+    effect = await effectCollection.find({postId: { $in: param.split(",") }}).toArray();
+  } else if (req.query.creatorId !== undefined){
+    param = req.query.creatorId;
+    effect = await effectCollection.find({creatorId: param}).toArray();
+  }
 
   res.json({
     list : effect
