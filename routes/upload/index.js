c85559b4ab80b3fc31a4a689b4745e6feaaca47c
songhwee1 <songhwee1@naver.com>
2022-06-25 21:47:17 +0900

    add image upload

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -1,9 +1,24 @@
 const express = require("express"); // module for express framework
 const { effectCollection, postIdCollection } = require("../../db");
 const router = express.Router({ mergeParams: true });
+const multer = require("multer");
+const path = require("path");
 
-router.post("/", async (req, res) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/",  upload.single("image"), async (req, res) => {
   const { title, content, userId, category, parentPostId } = req.body;
+  const imageUrl = `/images/${req.file.filename}`
 
   const effect = {
     title,
@@ -11,6 +26,7 @@ router.post("/", async (req, res) => {
     creatorId: userId,
     category,
     parentPostId,
+    imageUrl
   }
 
   await postIdCollection.updateOne({type: 'postId'}, {$inc: {value: 1}}, {upsert: true})
@@ -20,7 +36,19 @@ router.post("/", async (req, res) => {
   res.json({postId}).status(200);
 });
 
-router.post("/image", (req, res) => {
+//* If json, image come seperately as a request
+router.post("/image",  upload.single("image"), async (req, res) => {
+  const imageUrl = `/images/${req.file.filename}`
+
+  const effect = {
+    imageUrl
+  }
+
+  await postIdCollection.updateOne({type: 'postId'}, {$inc: {value: 1}}, {upsert: true})
+  const postId = (await postIdCollection.findOne({type: 'postId'})).value + ''
+  await effectCollection.insertOne({...effect, postId: postId})
+
+  res.json({postId}).status(200);
 });
 
 module.exports = router;
